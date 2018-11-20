4ba4e4c95f4f18210b62bd3c464606ef8832bd7a
Ashwintrn <ashwin@rently.com>
2018-11-20 18:57:38 +0530

    externally link given

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 require File.expand_path('lib/omniauth/strategies/Crudstart', Rails.root)
+CUSTOM_PROVIDER_URL = 'http://localhost:3000'
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :Crudstart , ENV["OAUTH_ID"], ENV["OAUTH_SECRET"]
 end
